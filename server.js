f11db72e18eb4ced4c3b5a2c26990021e2289a6a
Petrik Hunt <petrik.hunt@tktk.ee>
2025-12-15 22:12:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const app = express();
 const randomRouter = require('./routes/randomRecipe');
 
 app.use(express.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use('/recipes', recipseRoutes);
 app.use('/ingredients', ingredientsRoutes);
